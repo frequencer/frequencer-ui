f748e7886a5b0dab9f480224814909cf3590502d
James Rowley <jrowley@marcusengineering.com>
2021-09-19 09:02:26 -0700

    pmd stuff II

--- main.c
+++ main.c
@@ -12,9 +12,21 @@ main (void)
 	gpio_init();
 	pmp_init();
 
+	DS_RST_SetLow();
+	__delay_us(12);
+	DS_RST_SetHigh();
+	__delay_ms(125);
+
+	uint8_t did[3] = { 0 };
+	pmp_write(0, 0x04);
+	pmp_read(1);
+	did[0] = pmp_read(1);
+	did[1] = pmp_read(1);
+	did[2] = pmp_read(1);
+
 	while (1)
 	{
-		LED1_Set(ENC_SW_Value());
+		DS_BL_Set(ENC_SW_Value());
 		LED2_Set(ENC_A_Value());
 		LED3_Set(ENC_B_Value());
 	}
--- pmp_8080.c
+++ pmp_8080.c
@@ -8,7 +8,7 @@
 void
 pmp_init (void)
 {
-	
+	// nothing, we are just bit banging it right now
 }
 
 void
@@ -20,17 +20,33 @@ pmp_write (uint8_t address, uint8_t data)
 	}
 
 	PMA0_Set(address);
+	PMCS1_SetLow();
 	PMD_LAT = data;
+	PMWR_SetLow();
 	PMD_TRIS = 0x00;
+	PMWR_SetHigh();
+	PMD_TRIS = 0xFF;
+	PMCS1_SetHigh();
 }
 
 uint8_t
 pmp_read (uint8_t address)
 {
+	uint8_t out;
+
 	if (address > PMA_MAX)
 	{
-		return; // HANG_HERE
+		return 0; // HANG_HERE
 	}
 
-	return 0;
+	PMA0_Set(address);
+	PMCS1_SetLow();
+	PMD_TRIS = 0xFF;
+	PMRD_SetLow();
+	asm("nop");
+	out = PMD_PORT;
+	PMRD_SetHigh();
+	PMCS1_SetHigh();
+
+	return out;
 }
